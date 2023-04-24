b1c659d649dd551c61ada70fe7576e2ed421bb12
huz <>
2023-04-24 17:28:41 +0800

    sql脚本

---
+++ src/test/java/com/xqxls/mall/MallAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xqxls.mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
